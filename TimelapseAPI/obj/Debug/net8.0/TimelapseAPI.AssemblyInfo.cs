77a39a4730a991cbe4802a9e052e0a3dd1bd1154
DRUCK\PC <diegolandagil@gmail.com>
2026-05-23 19:12:39 +0200

    Comentario Store

--- TimelapseAPI/obj/Debug/net8.0/TimelapseAPI.AssemblyInfo.cs
+++ TimelapseAPI/obj/Debug/net8.0/TimelapseAPI.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("TimelapseAPI")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+902eab1d980c7f660e9d81a22aea48709e848566")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+90d435e3b0043a95536c2162459cd669cb1ce216")]
 [assembly: System.Reflection.AssemblyProductAttribute("TimelapseAPI")]
 [assembly: System.Reflection.AssemblyTitleAttribute("TimelapseAPI")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
---
+++ timelapse-vue/src/stores/useComentarioStore.ts
@@ -0,0 +1,51 @@
+import { ref } from 'vue'
+import { defineStore } from 'pinia'
+import { getAllComentarios, crearComentario, eliminarComentario, type Comentario } from '@/services/comentario'
+import { useAuthStore } from '@/stores/useAuthStore'
+
+export const useComentarioStore = defineStore('comentario', () => {
+  const comentarios = ref<Comentario[]>([])
+  const loading = ref(false)
+  const error = ref('')
+
+  //
+  async function fetchComentarios() {
+    loading.value = true
+    error.value = ''
+    try {
+      comentarios.value = await getAllComentarios()
+    } catch (e: any) {
+      error.value = e.message
+    } finally {
+      loading.value = false
+    }
+  }
+
+  //Recibe un string
+  async function crear(texto: string) {
+    const auth = useAuthStore()
+    if (!auth.usuario?.idUsuario) throw new Error('No hay usuario logueado')
+    error.value = ''
+    try {
+      const nuevo = await crearComentario({ texto, idUsuario: auth.usuario.idUsuario })
+      comentarios.value.unshift(nuevo)
+    } catch (e: any) {
+      error.value = e.message
+      throw e
+    }
+  }
+
+  //Recibe un id
+  async function eliminar(id: number) {
+    error.value = ''
+    try {
+      await eliminarComentario(id)
+      comentarios.value = comentarios.value.filter(c => c.idComentario !== id)
+    } catch (e: any) {
+      error.value = e.message
+      throw e
+    }
+  }
+
+  return { comentarios, loading, error, fetchComentarios, crear, eliminar }
+})
